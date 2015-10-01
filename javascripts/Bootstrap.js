37d27d10852fda2bd9d5e2106f51fbbf00242538
vernkennedy <vzidana14@wooster.edu>
2015-09-30 21:01:39 -0700

    Analytics and stuff

--- javascripts/Bootstrap.js
+++ javascripts/Bootstrap.js
@@ -1,5 +1,5 @@
-var app = angular.module('myApp', []);
-app.controller('customersCtrl', function($scope, $http) {
+var app = angular.module('myPortifolio', []);
+app.controller('projectsCtrl', function($scope, $http) {
   $http.get("https://raw.githubusercontent.com/vernkennedy/BootstrapProjects/gh-pages/javascripts/Bootstrap.json")
-  .success(function (response) {$scope.names = response.records;});
+  .success(function (response) {$scope.names = response.projects;});
 });
