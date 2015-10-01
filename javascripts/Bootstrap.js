54baa23d884a78006b691989204f30ca35c31f64
vernkennedy <vzidana14@wooster.edu>
2015-09-30 21:40:33 -0700

    Analytics and Tag Manager

--- javascripts/Bootstrap.js
+++ javascripts/Bootstrap.js
@@ -1,5 +1,5 @@
 var app = angular.module('myPortfolio', []);
 app.controller('projectsCtrl', function($scope, $http) {
   $http.get("https://raw.githubusercontent.com/vernkennedy/BootstrapProjects/gh-pages/javascripts/Bootstrap.json")
-  .success(function (response) {$scope.names = response.projects;});
+  .success(function (response) {$scope.names = response.records;});
 });
